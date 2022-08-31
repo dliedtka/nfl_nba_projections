4fbc1106f341f77366eb28b4270cbef138367dfe
David Liedtka <dliedtka@gmail.com>
2022-08-30 23:13:36 -0400

    fit linear regression model for qb seasons played

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nfl_nba_projections
+
+Based on my 2018 IT452: Advanced Web and Internet Systems project. Project an NFL/NBA career based on user-entered biometrics and combine statistics. Results are for fun and are tongue-in-cheek; the data is skewed towards... professional athletes.
+
+Doing NFL projections first.
--- nfl/build_models.py
+++ nfl/build_models.py
@@ -3,6 +3,8 @@
 
 import os
 import pickle
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
@@ -23,7 +25,7 @@ def quarterback_data():
     qb_data["link"] = []
     qb_data["draft_pos"] = []
     qb_data["seasons"] = []
-    # combine stats
+    qb_data["combine"] = []
     # career stats (passing, rushing)
     
     for idx in range(len(data["tracked_players"])):
@@ -46,29 +48,68 @@ def quarterback_data():
             # 1 index for link
             qb_data["link"].append(data["tracked_players"][idx][1])
             qb_data["draft_pos"].append(data["draft_pick"][idx])
+            # don't need position, ignoring age (less fun)
+            qb_data["combine"].append(data["combine_stats"][idx][2:])
 
     # debugging
-    total = 0
-    count = 0
-    for i in range(len(qb_data["name"])):
-        print (qb_data["name"][i], qb_data["draft_pos"][i], qb_data["seasons"][i])
-        if qb_data["seasons"][i] >= 0:
-            total += qb_data["seasons"][i]
-            count += 1
-    print (f"Average of {total/count} seasons played by {count}/{len(qb_data['name'])} qualifying QBs.")
+    #for i in range(len(qb_data["name"])):
+    #    print (qb_data["name"][i], qb_data["draft_pos"][i], qb_data["seasons"][i], qb_data["combine"][i])
+    
+    # seasons played dataset (nan if exercise not performed)
+    if not os.path.exists(f"{cur_dir}/data/qb_seasons.pkl"):
+        x = []
+        y = []
+        for i in range(len(qb_data["name"])):
+            if qb_data["seasons"][i] >= 0:
+                x.append(qb_data["combine"][i])
+                y.append([qb_data["seasons"][i], qb_data["draft_pos"][i]])
+        x = np.array(x, dtype=float)
+        y = np.array(y, dtype=float)
+
+        with open(f"{cur_dir}/data/qb_seasons.pkl", "wb") as fout:
+            pickle.dump((x,y), fout)
     
 
 def quarterback_predict():
     '''
-    Will want to predict seasons played.
+    Will want to predict seasons played. (normalize data)
     Then, for each season: 
     -games started, passing attempts (per game?) passing yards (per game?), passing TDs (per game?), passing INTs (per game?)
     -rushing attempts (per game?), rushing yards (per game?), rushing TDs (per game)
     -sacks (per game?), fumbles (per game?)
     -passer rating?
     '''
-    pass
+    # load x,y from seasons
+    with open(f"{cur_dir}/data/qb_seasons.pkl", "rb") as fin:
+        x, y = pickle.load(fin)
+    
+    # get medians (not including nan) for each exercise 
+    medians = []
+    for col in range(x.shape[1]):
+        vals = list(filter(lambda x: not np.isnan(x), x[:,col]))
+        vals.sort()
+        medians.append(vals[len(vals)//2])
+    #print (medians)
+
+    # one hot cols
+    one_hot = np.isnan(x).astype(float)
+    #print (one_hot)
+
+    # replace nan with medians
+    #print (x)
+    for col, median in enumerate(medians):
+        x[:,col][np.nonzero(one_hot[:,col])] = median
+    #print (x)
+
+    # add one hot cols to data
+    x = np.hstack((x, one_hot))
+    #print (x)
+
+    # train, predict
+    model = LinearRegression()
+    model.fit(x, y)     
 
 
 if __name__ == "__main__":
     quarterback_data()
+    quarterback_predict()
